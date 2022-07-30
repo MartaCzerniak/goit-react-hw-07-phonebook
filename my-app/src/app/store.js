c13297075cb0ecf8f53ea13e6d0a95759391448b
MartaCzerniak <86880796+MartaCzerniak@users.noreply.github.com>
2022-07-30 16:52:40 +0200

    .

--- my-app/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import newReducer from '../features/contactsSlice';
-
-export const store = configureStore({
-  reducer: {
-    data: newReducer,
-  },
-});
--- my-app/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const baseURL = 'https://624456ff3da3ac772b0e8aee.mockapi.io/';
-
-const api = axios.create({
-    baseURL
-})
-
-export default api;
