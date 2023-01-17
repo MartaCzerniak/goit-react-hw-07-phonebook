64c00b2def5a317c7c1642abe4ee11d805dd503b
MartaCzerniak <86880796+MartaCzerniak@users.noreply.github.com>
2023-01-17 13:24:41 +0100

    Updates

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-function Button({ label, type, onClick }) {
-  return (
-    <button type={type} onClick={onClick}>
-      {label}
-    </button>
-  );
-}
-
-export default Button;
--- src/redux/selectors.js
+++
@@ -1,5 +0,0 @@
-export const selectContacts = (state) => state.contacts;
-export const selectItems = (state) => state.contacts.items;
-export const selectFilter = (state) => state.contacts.filter;
-export const selectIsLoading = (state) => state.contacts.isLoading;
-export const selectError = (state) => state.contacts.error;
